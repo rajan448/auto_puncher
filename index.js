cf0ab1394062b2733f7a9d8a6aa918d835e6db42
rajan <rajank@dewsolutions.in>
2020-08-24 13:44:39 +0530

    Emulate Indian timezone

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer');
 const moment = require('moment');
+const sgMail = require('@sendgrid/mail');
 
-
+sgMail.setApiKey(process.env.SGKEY);
 const browserPromise = puppeteer.launch({
     args: [
         '--no-sandbox',
@@ -9,19 +10,13 @@ const browserPromise = puppeteer.launch({
 })
 
 exports.screenshot = async (req, res) => {
-
-    const url = 'http://corp.dewsolutions.in/lms/symfony/web/index.php/auth/login';
-
     const browser = await browserPromise;
     const context = await browser.createIncognitoBrowserContext();
     const page = await context.newPage();
 
-    await page.setViewport({
-        width: 1920,
-        height: 1080,
-    });
+    await page.emulateTimezone('Asia/Kolkata');
 
-    await page.goto(url);
+    await page.goto(process.env.PORTAL_URL);
     await page.waitForSelector('#btnLogin', { visible: true, timeout: 0 });
 
     // Login to the portal
@@ -33,43 +28,59 @@ exports.screenshot = async (req, res) => {
 
     // Punch In/Out
 
-    await page.goto('http://corp.dewsolutions.in/lms/symfony/web/index.php/attendance/punchIn');
-    // await page.waitForSelector('.punchInbutton', {visible: true, timeout: 0});
+    await page.goto(process.env.PUNCHIN_URL);
     const today = new moment(getISTTime());
-    
+
     isWeekDay = today.day() < 6 && today.day() > 0;
-    isActiveTime = today.isSameOrAfter(moment('9:00','HH:mm')) && today.isSameOrBefore(moment('18:00','HH:mm'))
+    isActiveTime = today.isSameOrAfter(moment('9:00', 'HH:mm')) && today.isSameOrBefore(moment('18:00', 'HH:mm'))
 
     try {
         await page.waitForSelector('.punchInbutton', {
-            timeout: 1000
+            timeout: 500
         });
-        if(isWeekDay && isActiveTime) {
+        if (isWeekDay && isActiveTime) {
             await page.click('.punchInbutton');
             await page.waitForSelector('.punchOutbutton', { visible: true, timeout: 0 });
+            sgMail.send(getEmailPayload(true));
         }
     } catch (err) {
-        if(!isActiveTime) {
+        if (!isActiveTime) {
             await page.click('.punchOutbutton');
             await page.waitForSelector('.punchInbutton', { visible: true, timeout: 0 });
+            sgMail.send(getEmailPayload(false));
         }
     }
-
-    const image = await page.screenshot({ fullPage: true });
-
 
     // Logout from the portal
     await (page.click('#welcome'));
     await page.waitForSelector('#welcome-menu', { visible: true });
     await page.click('#welcome-menu > ul:nth-child(1) > li:nth-child(3) > a:nth-child(1)');
 
-    res.setHeader('Content-type', 'image/png');
-    res.send(image);
-
-    context.close();
+    await context.close();
 }
 
 const getISTTime = () => {
     let d = new Date()
     return d.getTime() + (5.5 * 60 * 60 * 1000)
+}
+
+
+const getEmailPayload = (success) => {
+    return {
+        to: process.env.TO,
+        from: process.env.FROM,
+        subject: success
+            ? 'Login | ' + 'Your Today\'s Attendance'
+            : 'Logout | ' + 'Your Today\'s Attendance',
+        text: success
+            ? `Hi There,
+                We have successfully logged you in at ` + new Date(getISTTime()).toDateString()
+            : `Hi There, 
+                We have successfully logged you in at ` + new Date(getISTTime()).toDateString(),
+        html: success
+            ? `<div> Hi There,
+            We have successfully logged you in at ${new Date(getISTTime()).toDateString()} </div>`
+            : `Hi There, 
+            We have successfully logged you in at ${new Date(getISTTime()).toDateString()} </div>`
+    }
 }
